0225f966e54fe0a99679101f88a6bc2fc604e715
Anurag <anuraggautam876@gmail.com>
2022-05-17 21:41:56 +0530

    Second commit

---
+++ M1.js
@@ -0,0 +1,11 @@
+// Ways to print in javascript
+
+console.log("hello World");
+alert('Hello from my side');
+document.write("I am wishing you toooooooo!!!!!!");
+
+//Javascript Console API
+
+console.log("Hello World");
+console.warn("This is Warning");
+console.error("This is an error");
