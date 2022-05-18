4af6b08897fcc400f1c205471eb2153031ea11b6
Anurag <anuraggautam876@gmail.com>
2022-05-18 22:07:28 +0530

    All in One

--- M1.js
+++ M1.js
@@ -29,3 +29,23 @@ console.log(myArr);
 
 
 //String methods
+/*
+let myString = "You are Anurag!! Right ?"
+console.log(myString.length);
+console.log(myString.indexOf("You"));
+console.log(myString.indexOf("are"));
+console.log(myString.lastIndexOf("Right"));
+console.log(myString.slice(0,3));
+console.log(myString.replace("Anurag", "Sunny"));
+*/
+
+
+// ####   Dates in Javascript   ####
+/*
+let myDate = new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+*/
