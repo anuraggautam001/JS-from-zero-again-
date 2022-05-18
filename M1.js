a1d10955bec645d9c4522c29e34511e3617e20f9
Anurag <anuraggautam876@gmail.com>
2022-05-18 21:55:50 +0530

    All in one

--- M1.js
+++ M1.js
@@ -1,11 +1,31 @@
-// Ways to print in javascript
+// // Ways to print in javascript
 
-console.log("hello World");
-alert('Hello from my side');
-document.write("I am wishing you toooooooo!!!!!!");
+// console.log("hello World");
+// alert('Hello from my side');
+// document.write("I am wishing you toooooooo!!!!!!");
 
-//Javascript Console API
+// //Javascript Console API
 
-console.log("Hello World");
-console.warn("This is Warning");
-console.error("This is an error");
+// console.log("Hello World");
+// console.warn("This is Warning");
+// console.error("This is an error");
+
+// Arrays Methods
+/*
+let myArr = ["Fan", "Camera", 34, null, true];
+
+console.log(myArr.length);
+myArr.pop();
+Output :-- (4) ['Fan', 'Camera', 34, null]
+myArr.push("Anurag");
+output :-- (6) ['Fan', 'Camera', 34, null, true, 'Anurag']
+myArr.shift();
+output :-- (4) ['Camera', 34, null, true]
+myArr.unshift("Anurag");
+output :-- (6) ['Anurag', 'Fan', 'Camera', 34, null, true]
+console.log(myArr);
+
+*/
+
+
+//String methods
